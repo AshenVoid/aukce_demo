0894bd0221c8e2dd0a4f14fd9008e804806d2fab
AshenVoid <richardegydy@gmail.com>
2025-06-01 23:11:22 +0200

    Models - User

--- hlavni/models.py
+++ hlavni/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    city = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    STATUS_CHOICES = [
+        ('AKTIVNI', 'Aktivní'),
+        ('NEAKTIVNÍ', 'Neaktivní'),
+        ('BLOKOVANÝ', 'Blokovaný')
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='AKTIVNI')
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    TYP_CHOICES = [
+        ('BEZNY', 'Bezny'),
+        ('PREMIUM', 'Premium'),
+    ]
+    typ = models.CharField(max_length=10, choices=TYP_CHOICES, default='BEZNY')
+
+    def __str__(self):
+        return self.username
+
