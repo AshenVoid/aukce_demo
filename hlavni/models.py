3282afe30d109db3ec07531943d0fbee67395fe0
AshenVoid <richardegydy@gmail.com>
2025-06-01 23:27:45 +0200

    Models - Bids

--- hlavni/models.py
+++ hlavni/models.py
@@ -48,3 +48,13 @@ class Auction(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Bids(models.Model):
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bid = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.bid} on {self.auction}"
