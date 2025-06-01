202cce1d541aa653ad159382c00a9bcc569ce688
AshenVoid <richardegydy@gmail.com>
2025-06-02 00:16:43 +0200

    Small changes

--- aukce/settings.py
+++ aukce/settings.py
@@ -122,7 +122,7 @@ if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Static files
-STATIC_URL = '/static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
