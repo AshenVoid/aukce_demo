e04805dbab347d398ce3a522d4ad4d866e64eb92
AshenVoid <richardegydy@gmail.com>
2025-06-02 00:21:28 +0200

    Small changes

--- aukce/settings.py
+++ aukce/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-from decouple import config
-SECRET_KEY = config("DJANGO_SECRET_KEY",)
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
 
+DEBUG = True
 
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -111,15 +115,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-import os
-
-# Debug pouze lokálně
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
-
-RENDER_EXTERNAL_HOSTNAME = os.getenv('RENDER_EXTERNAL_HOSTNAME')
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Static files
 STATIC_URL = "static/"
