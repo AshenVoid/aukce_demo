d47a62cc9472c97315108c9d9cfb8fee025d674f
AshenVoid <richardegydy@gmail.com>
2025-06-01 23:43:54 +0200

    Registered models

--- hlavni/admin.py
+++ hlavni/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category, User, Auction, Bids, Tracking, Review
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(User)
+admin.site.register(Auction)
+admin.site.register(Bids)
+admin.site.register(Tracking)
+admin.site.register(Review)
+
