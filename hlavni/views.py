bb33db0bdec922ca7ab65ca8c66f8321a915cc9b
AshenVoid <richardegydy@gmail.com>
2025-06-02 11:11:45 +0200

    Html - Přehození register logiky na Django forms

--- hlavni/views.py
+++ hlavni/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import logout
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from .models import Auction
 
@@ -12,7 +13,14 @@ def login_view(request):
 
 
 def register_view(request):
-    return render(request, 'hlavni/register.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            form = UserCreationForm()
+        return render(request, 'hlavni/register.html', {'form': form})
 
 
 def auction_detail(request):
