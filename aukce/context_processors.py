9ea2ee4915eb1d1e2677ed12dd2ddf9a53d3f26b
AshenVoid <richardegydy@gmail.com>
2025-06-02 11:04:57 +0200

    Years

---
+++ aukce/context_processors.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+
+
+def current_year(request):
+    return {'year': datetime.now().year}
--- aukce/settings.py
+++ aukce/settings.py
@@ -62,6 +62,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "aukce.context_processors.current_year",
             ],
         },
     },
