c8e9d6a606e5af64c11c131386e551c06cea1971
AshenVoid <richardegydy@gmail.com>
2025-06-02 11:08:42 +0200

    Html - Přehození login logiky na Django forms

--- hlavni/urls.py
+++ hlavni/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('', views.home, name='index'),
-    path('login/', views.login_view, name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='hlavni/login.html'), name='login'),
     path('register/', views.register_view, name='register'),
     path('aukce/<int:id>/', views.auction_detail, name='aukce_detail'),
     path('aukce/<int:id>/bid/', views.bid, name='bid'),
